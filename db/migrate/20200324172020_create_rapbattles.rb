6528b52d796f69103cb333b5e54b4344a9267b79
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-24 19:05:15 -0500

    Change field in rapbattles to :challengee_id

--- db/migrate/20200324172020_create_rapbattles.rb
+++ db/migrate/20200324172020_create_rapbattles.rb
@@ -2,7 +2,7 @@ class CreateRapbattles < ActiveRecord::Migration[6.0]
   def change
     create_table :rapbattles do |t|
       t.integer :challenger_id
-      t.integer :challenge_id
+      t.integer :challengee_id
       t.integer :venue_id
       t.integer :challenger_points
       t.integer :challengee_points
