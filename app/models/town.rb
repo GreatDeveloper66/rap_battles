ae522fb7fd3371a177fb1af01816a76b4a95edb2
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-24 19:29:16 -0500

    Add challenge method to User

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,5 +1,6 @@
 class Town < ApplicationRecord
   has_many :venues
+  
   def addVenue(name,points)
     town_id = self.id
     Venue.create(name: name,points:points,town_id: town_id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_many :challengees, through: :challenged_users
   has_many :challenging_users, foreign_key: 'challengee_id', class_name: 'Rapbattle'
   has_many :challengers, through: :challenging_users
+
+  def challenge(another_user)
+    
+  end
 end
