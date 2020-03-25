0047c0cca4a880b28f347a913ecdfaf763c51f1b
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-24 19:29:00 -0500

    Fix syntax bugs in seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,29 +6,29 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
-Venue.destory_all
+Venue.destroy_all
 Town.destroy_all
 Rapbattle.destroy_all
 Trashbattle.destroy_all
 Trashtalk.destroy_all
 rappers = ["Snoop Dog","Ice Cube","Vanilla Ice","Tone Loc","Jay-Z","Sir Mix-a-Lot","LL Cool J","Eminem"]
-trash = [["You went to a private school",3]
-          ,["You live in a trailer park",5]
-          ,"You mamma is so fat Dora can't explore her",7]
-          ,["You mamma is so ugly people go as her for Halloween",8]
-          ,["You mamma is so stupid she tried to climb mountain dew",8]
-          ,["I'm sorry I hurt your feelings when I called you stupid. I thought you already knew.",10],
-          ,["I'd like to see things from your point of view but I can't get my head that far up my ass",10],
-          ,["You must have been born on a highway because that's where most accidents happen",10]
-          ,["Wow you're even dumber than you look",6]
-          ,["When I see your face there's not a single thing I would change except the direction I'm walking in", 9],
-          ,["You mama is so fat when she got in the scale it said, I need your weight not your zip code",7]
-          ,["You mamma is so stupid she returned a donut because it had a hole in it",8]
+trash = [["You went to a private school",3],
+          ["You live in a trailer park",5],
+          ["You mamma is so fat Dora can't explore her",7],
+          ["You mamma is so ugly people go as her for Halloween",8],
+          ["You mamma is so stupid she tried to climb mountain dew",8],
+          ["I'm sorry I hurt your feelings when I called you stupid. I thought you already knew.",10],
+          ["I'd like to see things from your point of view but I can't get my head that far up my ass",10],
+          ["You must have been born on a highway because that's where most accidents happen",10],
+          ["Wow you're even dumber than you look",6],
+          ["When I see your face there's not a single thing I would change except the direction I'm walking in", 9],
+          ["You mama is so fat when she got in the scale it said, I need your weight not your zip code",7],
+          ["You mamma is so stupid she returned a donut because it had a hole in it",8]
         ]
 towns = [["Chicago",5,"/app/assets/images/chicago.jpg"],
          ["New York",10,"/app/assets/images/newyork.jpg"],
          ["Los Angeles",15,"/app/assets/images/losangeles.jpg"],
-         ["Detroit",20,"/app/assets/images/detroit.jpg"}
+         ["Detroit",20,"/app/assets/images/detroit.jpg"]
          ]
 
 def randomString
@@ -57,7 +57,6 @@ losangeles.addVenue("Hollywood Bowl",22)
 detroit.addVenue("Filmore Detroit",55)
 detroit.addVenue("Saint Andrews Hall",45)
 
-
 trash.each do |t|
-  t.create(name:t[0],points:t[1])
+  Trashtalk.create(snippet:t[0],points:t[1])
 end
