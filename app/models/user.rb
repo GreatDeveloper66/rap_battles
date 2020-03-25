f061c5c06502863a42d46b05c6872e63cef07105
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-24 20:17:37 -0500

    Update challenge method in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,31 @@ class User < ApplicationRecord
   has_many :challenging_users, foreign_key: 'challengee_id', class_name: 'Rapbattle'
   has_many :challengers, through: :challenging_users
 
-  def challenge(another_user)
-    
+  has_secure_password
+  validates :username, uniqueness: { case_sensitive: false }
+
+  def challenge(another_user, fight_at_venue)
+
+    challenger_array = []
+    challengee_array = []
+    5.times do
+      challenger_array << Trashtalk.all.to_a.sample
+      challengee_array << Trashtalk.all.to_a.sample
+    end
+    challenger_points = challenger_array.reduce(0) { |sum,c| sum + c.points }
+    challengee_points = challengee_array.reduce(0) { |sum,c| sum + c.points }
+
+
+    Rapbattle.create(
+      challenger_id: self.id,
+      challengee_id: another_user.id,
+      venue_id: fight_at_venue.id,
+      challenger_points: challenger_points,
+      challengee_points: challengee_points
+    )
+
+    (challenger_array + challengee_array).each do |c|
+      Trashbattle.create(trashtalk_id: c.id, rapbattle_id: self.id)
+    end
   end
 end
