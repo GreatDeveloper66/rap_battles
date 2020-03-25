9f863d2335ba2861bc9a62fb308d93a79b27fdb4
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-25 11:27:41 -0500

    Add routes for login pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #resources :users, only: [:show,:index,:new,:create]
+  resources :users, only: [:show,:index,:new,:create]
   resources :venues, only: [:show,:index,:new,:create]
   resources :towns, only: [:show,:index,:new,:create]
   resources :trashtalks, only: [:show, :index, :new, :create]
   resources :rapbattles, only: [:show,:index,:new,:create]
+  resources :sessions, only: [:new,:show,:index,:new,:create]
   get '/', to: 'sessions#home', as: 'home'
 end
