b2178726c750d06928c4ee92d6290e956735f505
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-25 12:53:58 -0500

    Add destroy, create, new methods to SessionController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,22 @@
 class SessionsController < ApplicationController
+  def destroy
+    session[:user_id] = nil
+    redirect_to users_path
+  end
+
   def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to rapbattles_home_path
+    else
+      flash[:error] = "Invalid credentials"
+      redirect_to login_path
+    end
   end
   def home
   end
