46b804b0c0b6baf0e763f6a1ec628d4b4f4769ef
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-25 14:27:15 -0500

    Change Redirect path. Change User.find_by params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
-    redirect_to users_path
+    redirect_to home_path
   end
 
   def new
@@ -9,10 +9,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(username: params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to rapbattles_home_path
+      redirect_to users_path
     else
       flash[:error] = "Invalid credentials"
       redirect_to login_path
