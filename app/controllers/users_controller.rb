430af0f253905f7250f19e47265ac815ab59d23a
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-25 12:54:25 -0500

    Add new and create methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to :rapbattles_home_path
+    else
+      render :new
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name,:username,:email,:password)
   end
 end
