5562e0645e9bb1b3b12f6f3b54048406cf47f54f
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-25 14:27:40 -0500

    Modify redirect_path for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,10 +7,13 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to :rapbattles_home_path
+      redirect_to users_path
     else
+      @user = User.new
       render :new
     end
+  end
+  def index
   end
   private
   def user_params
