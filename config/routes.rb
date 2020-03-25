65eb842d02e77f8eebe6e0e476b589bebfbf4fe8
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-24 20:17:04 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #resources :users, only: [:show,:index,:new,:create]
+  resources :venues, only: [:show,:index,:new,:create]
+  resources :venues, only: [:show,:index,:new,:create]
+  resources :trashtalks, only: [:show, :index, :new, :create]
+  resources :rapbattles, only: [:show,:index,:new,:create]
+
 end
