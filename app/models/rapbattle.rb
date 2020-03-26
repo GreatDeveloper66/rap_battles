ce6945375ce38f5dbf6e7e98a094989488d818a1
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-26 11:13:58 -0500

    Add a few stats to rapbattles index page

--- app/models/rapbattle.rb
+++ app/models/rapbattle.rb
@@ -13,4 +13,11 @@ class Rapbattle < ApplicationRecord
   def self.total_trashtalks
     self.all.reduce(0) { |sum,r| sum + r.trashtalks.to_a.count }
   end
+  def self.most_popular_trash
+    trashes = self.all.map{ |s| s.trashtalks.to_a }
+    trashes = trashes.flatten
+    trashes = trashes.map{ |t| t.snippet}
+    freq = trashes.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+    trashes.max_by { |v| freq[v] }
+  end
 end
