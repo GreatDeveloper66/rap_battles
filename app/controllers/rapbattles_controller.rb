a5de78a2a6715da825b3a37b1484ad12cc315817
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-25 20:15:05 -0500

    Add code to new method for rap battles

--- app/controllers/rapbattles_controller.rb
+++ app/controllers/rapbattles_controller.rb
@@ -2,6 +2,11 @@ class RapbattlesController < ApplicationController
   def home
   end
   def new
+    @rapbattle = Rapbattle.new
+    @venues = Venue.all.to_a
+    @rappers = User.all.to_a.select do |u|
+      u.id != session[:user_id]
+    end
   end
   def index
   end
