9dc92fd179c8bc898ffa40e0a8cb8a2fdb35d45a
GreatDeveloper66 <ashaffe@gmail.com>
2020-03-26 09:45:08 -0500

    Add update method and route to Trashtalks

--- app/controllers/trashtalks_controller.rb
+++ app/controllers/trashtalks_controller.rb
@@ -13,7 +13,13 @@ class TrashtalksController < ApplicationController
     @trashtalk = Trashtalk.find_by(id: params[:id])
   end
   def update
-    
+    @trashtalk = Trashtalk.find_by(id: params[:id])
+    if @trashtalk.update(trashtalk_params)
+      redirect_to trashtalks_path
+    else
+      @trashtalk = Trashtalk.find_by(id: params[:id])
+      render :edit
+    end
   end
   private
   def trashtalk_params
